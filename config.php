ef1f713ac8bddb53b73ef30d36b99ae47f9b6c79
Douglas PSantoss <douglas.pereira300@gmail.com>
2024-06-05 20:12:55 -0300

    feat: validation form

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '1234';
+    $dbName = 'formulario-douglas';
+    
+    $conn = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    // if($conexao->connect_errno)
+    // {
+    //     echo "Erro";
+    // }
+    // else
+    // {
+    //     echo "Conexão efetuada com sucesso";
+    // }
+
+?>
