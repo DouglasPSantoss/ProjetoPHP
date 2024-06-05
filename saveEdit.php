28f45a5f73d6caa6e70bf5c63f881c2619c8b47d
Douglas PSantoss <douglas.pereira300@gmail.com>
2024-06-05 20:54:23 -0300

    edit v3

--- saveEdit.php
+++ saveEdit.php
@@ -17,7 +17,7 @@
         $sqlInsert = "UPDATE usuarios 
         SET nome='$nome',senha='$senha',email='$email',telefone='$telefone',sexo='$sexo',data_nasc='$data_nasc',cidade='$cidade',estado='$estado',endereco='$endereco'
         WHERE id=$id";
-        $result = $conexao->query($sqlInsert);
+        $result = $conn->query($sqlInsert);
         print_r($result);
     }
     header('Location: sistema.php');
